da589b49b0e107c614f92ade0323319ea8e80424
Alpay Aygün <alpayaygun40@gmail.com>
2020-06-06 17:29:00 +0300

    vuforia added

---
+++ Assets/RobotKontrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RobotKontrol : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Animation anim;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+    }
+
+    
+    void Update()
+    {
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 hareket = new Vector3(x, 0, y);
+        rb.velocity = hareket * 1f;
+
+        if(x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+
+        }
+        if(x != 0 || y != 0)
+        {
+            anim.Play("idle");
+        }
+       
+    
+    }
+}
